4d11fe7a59682c3ac660594f902b14b68c754fcf
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-04-28 20:46:26 +0700

    test LiveData.map

--- mvvm/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/LiveDataTest.kt
+++ mvvm/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/LiveDataTest.kt
@@ -57,4 +57,58 @@ class LiveDataTest : BaseTestsClass() {
             assertEquals(expected = 3, actual = observedCounter, message = msg)
         }
     }
+
+    @Test
+    fun `live data map testing`() {
+        val ld: MutableLiveData<Int> = MutableLiveData(initialValue = 1)
+        val mapLd: LiveData<Int> = ld.map { it * -1 }
+
+        var observedCounter = 0
+        var lastValue: Int? = null
+        val observer: (Int) -> Unit = {
+            observedCounter++
+            lastValue = it
+        }
+        mapLd.addObserver(observer)
+
+        "we should got initial value".let { msg ->
+            assertEquals(expected = 1, actual = ld.value, message = msg)
+            assertEquals(expected = -1, actual = mapLd.value, message = msg)
+            assertEquals(expected = -1, actual = lastValue, message = msg)
+            assertEquals(expected = 1, actual = observedCounter, message = msg)
+        }
+
+        ld.value = 2
+        "we should got changes".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = -2, actual = mapLd.value, message = msg)
+            assertEquals(expected = -2, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.value = 2
+        "value set but not changed, we should not got changes".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = -2, actual = mapLd.value, message = msg)
+            assertEquals(expected = -2, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.postValue(3)
+        "postValue should set value on mainThread, so change should be applied".let { msg ->
+            assertEquals(expected = 3, actual = ld.value, message = msg)
+            assertEquals(expected = -3, actual = mapLd.value, message = msg)
+            assertEquals(expected = -3, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+
+        mapLd.removeObserver(observer)
+        ld.value = 4
+        "observer was removed - we should not got any changes".let { msg ->
+            assertEquals(expected = 4, actual = ld.value, message = msg)
+            assertEquals(expected = -4, actual = mapLd.value, message = msg)
+            assertEquals(expected = -3, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+    }
 }
