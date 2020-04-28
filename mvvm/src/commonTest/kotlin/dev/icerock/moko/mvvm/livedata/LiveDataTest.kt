263dc39809100d832b200d14bacf97409bcbc879
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-04-28 22:03:23 +0700

    Add LiveData.distinct tests

--- mvvm/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/LiveDataTest.kt
+++ mvvm/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/LiveDataTest.kt
@@ -164,4 +164,66 @@ class LiveDataTest : BaseTestsClass() {
             assertEquals(expected = 3, actual = observedCounter, message = msg)
         }
     }
+
+    @Test
+    fun `live data distinct testing`() {
+        val ld: MutableLiveData<Int> = MutableLiveData(initialValue = 1)
+        val distinctLd: LiveData<Int> = ld.distinct()
+
+        var observedCounter = 0
+        var lastValue: Int? = null
+        val observer: (Int) -> Unit = {
+            observedCounter++
+            lastValue = it
+        }
+        distinctLd.addObserver(observer)
+
+        "we should got initial value".let { msg ->
+            assertEquals(expected = 1, actual = ld.value, message = msg)
+            assertEquals(expected = 1, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 1, actual = lastValue, message = msg)
+            assertEquals(expected = 1, actual = observedCounter, message = msg)
+        }
+
+        ld.value = 2
+        "we should got changes".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = 2, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 2, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.value = 2
+        "value set but not changed, we should not got any changes".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = 2, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 2, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.postValue(2)
+        "postValue set but not changed, we should not got any changes".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = 2, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 2, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.postValue(4)
+        "postValue set, we should got changes".let { msg ->
+            assertEquals(expected = 4, actual = ld.value, message = msg)
+            assertEquals(expected = 4, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 4, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+
+        distinctLd.removeObserver(observer)
+        ld.value = 5
+        "observer was removed - we should not got any changes".let { msg ->
+            assertEquals(expected = 5, actual = ld.value, message = msg)
+            assertEquals(expected = 5, actual = distinctLd.value, message = msg)
+            assertEquals(expected = 4, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+    }
 }
