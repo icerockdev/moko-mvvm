cfabbddac0557386e49eab7ec7b6e0713b0155ca
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-04-28 20:28:47 +0700

    core LiveData observing test

---
+++ mvvm/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/LiveDataTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.icerock.moko.mvvm.livedata
+
+import dev.icerock.moko.mvvm.BaseTestsClass
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class LiveDataTest : BaseTestsClass() {
+
+    @Test
+    fun `live data observer testing`() {
+        val ld: MutableLiveData<Int> = MutableLiveData(initialValue = 0)
+
+        var observedCounter = 0
+        var lastValue: Int? = null
+        val observer: (Int) -> Unit = {
+            observedCounter++
+            lastValue = it
+        }
+        ld.addObserver(observer)
+
+        "we should got initial value".let { msg ->
+            assertEquals(expected = 0, actual = ld.value, message = msg)
+            assertEquals(expected = 0, actual = lastValue, message = msg)
+            assertEquals(expected = 1, actual = observedCounter, message = msg)
+        }
+
+        ld.value = 1
+
+        "we should got changes".let { msg ->
+            assertEquals(expected = 1, actual = ld.value, message = msg)
+            assertEquals(expected = 1, actual = lastValue, message = msg)
+            assertEquals(expected = 2, actual = observedCounter, message = msg)
+        }
+
+        ld.postValue(2)
+
+        "postValue should set value on mainThread, so change should be applied".let { msg ->
+            assertEquals(expected = 2, actual = ld.value, message = msg)
+            assertEquals(expected = 2, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+
+        ld.removeObserver(observer)
+        ld.value = 3
+
+        "observer was removed - we should not got any changes".let { msg ->
+            assertEquals(expected = 3, actual = ld.value, message = msg)
+            assertEquals(expected = 2, actual = lastValue, message = msg)
+            assertEquals(expected = 3, actual = observedCounter, message = msg)
+        }
+    }
+}
