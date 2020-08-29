f7d479bafddfec94bbb5b9a5f76d44e2d326f503
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-08-30 02:09:43 +0700

    use substitution only in sample

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,6 @@ allprojects {
         maven { url = uri("https://dl.bintray.com/icerockdev/moko") }
     }
 
-    configurations.all {
-        resolutionStrategy.dependencySubstitution {
-            substitute(module(Deps.Libs.MultiPlatform.mokoMvvm))
-                .with(project(":mvvm"))
-        }
-    }
-
     plugins.withId(Deps.Plugins.androidLibrary.id) {
         configure<com.android.build.gradle.LibraryExtension> {
             compileSdkVersion(Deps.Android.compileSdk)
