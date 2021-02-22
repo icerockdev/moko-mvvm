531c8500fd0bf21d143e660c6550d9294cec2faf
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-02-22 20:28:16 +0700

    #105 mediator compose test

--- mvvm-livedata/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/MediatorTest.kt
+++ mvvm-livedata/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/MediatorTest.kt
@@ -77,4 +77,65 @@ class MediatorTest {
             messagePrefix = "observer removed"
         )
     }
+
+    @Test
+    fun `compose validate`() {
+        val input1: MutableLiveData<Int> = MutableLiveData(initialValue = 1)
+        val input2: MutableLiveData<Int> = MutableLiveData(initialValue = 2)
+        val output: LiveData<Int> = MediatorLiveData(initialValue = 3)
+            .compose(input1, input2) { value1, value2 ->
+                value1 * value2
+            }
+        val observer = AssertObserver<Int>()
+        output.addObserver(observer)
+
+        assert(
+            input = input1,
+            output = output,
+            outputObserver = observer,
+            expectInput = 1,
+            expectOutput = 2,
+            expectLastObservedValue = 2,
+            expectObserveCount = 1,
+            messagePrefix = "initialization step"
+        )
+
+        input1.value = 2
+        assert(
+            input = input1,
+            output = output,
+            outputObserver = observer,
+            expectInput = 2,
+            expectOutput = 4,
+            expectLastObservedValue = 4,
+            expectObserveCount = 2,
+            messagePrefix = "first input changed"
+        )
+
+        input2.value = 3
+        assert(
+            input = input2,
+            output = output,
+            outputObserver = observer,
+            expectInput = 3,
+            expectOutput = 6,
+            expectLastObservedValue = 6,
+            expectObserveCount = 3,
+            messagePrefix = "second input changed"
+        )
+
+        output.removeObserver(observer)
+        input1.value = 5
+        input2.value = 7
+        assert(
+            input = input2,
+            output = output,
+            outputObserver = observer,
+            expectInput = 7,
+            expectOutput = 35,
+            expectLastObservedValue = 6,
+            expectObserveCount = 3,
+            messagePrefix = "observer removed"
+        )
+    }
 }
