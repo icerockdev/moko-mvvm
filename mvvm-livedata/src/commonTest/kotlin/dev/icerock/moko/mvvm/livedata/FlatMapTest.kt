df025e8433fca62b442409174362817b2b8dade6
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-02-22 13:14:50 +0700

    #105 flatMapBuffered test

--- mvvm-livedata/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/FlatMapTest.kt
+++ mvvm-livedata/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/FlatMapTest.kt
@@ -16,14 +16,14 @@ class FlatMapTest {
 
     @Test
     fun `flat map valid`() {
-        var counter: Int = 0
-        val input = MutableLiveData<Int>(initialValue = 0)
+        var counter = 0
+        val input = MutableLiveData(initialValue = 0)
         val output = input.flatMap { inputValue ->
             val newOutput = MutableLiveData(initialValue = inputValue)
             newOutput.value += counter++
             newOutput
         }
-        var observerCounter: Int = 0
+        var observerCounter = 0
         output.addObserver { observerCounter++ }
 
         assertEquals(expected = 0, actual = output.value)
@@ -40,10 +40,10 @@ class FlatMapTest {
 
     @Test
     fun `flat map shared`() {
-        val input = MutableLiveData<Int>(initialValue = 0)
+        val input = MutableLiveData(initialValue = 0)
         val internalLiveData = MutableLiveData(initialValue = 1)
         val output = input.flatMap { internalLiveData }
-        var observerCounter: Int = 0
+        var observerCounter = 0
         output.addObserver { observerCounter++ }
 
         assertEquals(expected = 1, actual = output.value)
@@ -76,5 +76,68 @@ class FlatMapTest {
         }
 
         sharedSource.value = 1
+    }
+
+    @Test
+    fun `flatMapBuffered validate`() {
+        val input: MutableLiveData<Int> = MutableLiveData(initialValue = 2)
+        val input2: MutableLiveData<Int> = MutableLiveData(initialValue = 1)
+        val output: LiveData<String> = input.flatMapBuffered { current, new ->
+            MutableLiveData("$current:$new").mergeWith(input2) { str, int ->
+                "$str:$int"
+            }
+        }
+        val observer = AssertObserver<String>()
+        output.addObserver(observer)
+
+        // TODO maybe we should change logic of flatMapBuffered at initialization step?
+        assert(
+            input = input,
+            output = output,
+            outputObserver = observer,
+            expectInput = 2,
+            expectOutput = "2:2:1",
+            expectLastObservedValue = "2:2:1",
+            expectObserveCount = 1,
+            messagePrefix = "initialization with null"
+        )
+
+        input.value = 3
+        assert(
+            input = input,
+            output = output,
+            outputObserver = observer,
+            expectInput = 3,
+            expectOutput = "2:3:1",
+            expectLastObservedValue = "2:3:1",
+            expectObserveCount = 2,
+            messagePrefix = "new value"
+        )
+
+        input2.value = 2
+        assert(
+            input = input,
+            output = output,
+            outputObserver = observer,
+            expectInput = 3,
+            expectOutput = "2:3:2",
+            expectLastObservedValue = "2:3:2",
+            expectObserveCount = 3,
+            messagePrefix = "new value"
+        )
+
+        output.removeObserver(observer)
+        input.value = 4
+        input2.value = 3
+        assert(
+            input = input,
+            output = output,
+            outputObserver = observer,
+            expectInput = 4,
+            expectOutput = "3:4:3",
+            expectLastObservedValue = "2:3:2",
+            expectObserveCount = 3,
+            messagePrefix = "change input after removing observer"
+        )
     }
 }
