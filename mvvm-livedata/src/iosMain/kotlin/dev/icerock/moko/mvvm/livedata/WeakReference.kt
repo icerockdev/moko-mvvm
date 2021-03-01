e8ff0afc6ec8151203df38c852e87ae3c5d568b4
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-03-01 11:48:36 +0700

    #105 remove unused weakref

--- mvvm-livedata/src/iosMain/kotlin/dev/icerock/moko/mvvm/livedata/WeakReference.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
- */
-
-package dev.icerock.moko.mvvm.livedata
-
-actual typealias WeakReference<T> = kotlin.native.ref.WeakReference<T>
