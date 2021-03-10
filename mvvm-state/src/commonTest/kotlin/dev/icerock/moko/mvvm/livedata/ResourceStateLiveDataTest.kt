5a0dbe72f75b03efd6964ba5cc6db8f3990cfd55
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-03-10 09:46:34 +0700

    #105 move State classes to different filenames - it allow us to use state and state-deprecated artifacts in single app for migration

--- mvvm-state/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/ResourceStateLiveDataTest.kt
+++ mvvm-state/src/commonTest/kotlin/dev/icerock/moko/mvvm/livedata/ResourceStateLiveDataTest.kt
@@ -11,7 +11,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class LiveDataTest {
+class ResourceStateLiveDataTest {
 
     @get:TestRule
     val instantTaskExecutorRule = AndroidArchitectureInstantTaskExecutorRule()
