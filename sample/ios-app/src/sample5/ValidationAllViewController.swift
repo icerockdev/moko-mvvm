dec1a28efbef56e8b0276c846226106e54426be4
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-03-15 22:10:18 +0700

    #105 fix ios sample - remove testing code

--- sample/ios-app/src/sample5/ValidationAllViewController.swift
+++ sample/ios-app/src/sample5/ValidationAllViewController.swift
@@ -2,16 +2,11 @@
  * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import Foundation
+import UIKit
 import MultiPlatformLibrary
-import MultiPlatformLibraryMvvm
-
-class MyTextField: UITextField {
-    
-}
 
 class ValidationAllViewController: UIViewController {
-    @IBOutlet private var emailField: MyTextField!
+    @IBOutlet private var emailField: UITextField!
     @IBOutlet private var passwordField: UITextField!
     @IBOutlet private var button: UIButton!
     
@@ -22,40 +17,12 @@ class ValidationAllViewController: UIViewController {
         
         viewModel = ValidationAllViewModel()
         
-        emailField.bindTextTwoWay(liveData: viewModel.email)
-        passwordField.bindTextTwoWay(liveData: viewModel.password)
-        button.bindEnabled(liveData: viewModel.isLoginButtonEnabled)
+        viewModel.email.bindStringTwoWayToTextFieldText(textField: emailField)
+        viewModel.password.bindStringTwoWayToTextFieldText(textField: passwordField)
+        viewModel.isLoginButtonEnabled.bindBoolToControlEnabled(control: button)
     }
     
     override func didMove(toParent parent: UIViewController?) {
         if(parent == nil) { viewModel.onCleared() }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.navigationController?.popViewController(animated: true)
-        }
-    }
-}
-
-class StartViewController: UIViewController {
-    private var counter: Int = 0
-    @IBOutlet var openButton: UIButton!
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if counter > 10 {
-            GarbageCollectorKt.collect()
-            GarbageCollectorKt.cycles()
-            return
-        }
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.counter += 1
-            self?.openButton.sendActions(for: .touchUpInside)
-        }
     }
 }
--- sample/mpp-library/src/commonMain/kotlin/com/icerockdev/library/sample5/ValidationAllViewModel.kt
+++ sample/mpp-library/src/commonMain/kotlin/com/icerockdev/library/sample5/ValidationAllViewModel.kt
@@ -6,35 +6,16 @@ package com.icerockdev.library.sample5
 
 import dev.icerock.moko.mvvm.livedata.LiveData
 import dev.icerock.moko.mvvm.livedata.MutableLiveData
-import dev.icerock.moko.mvvm.livedata.flatMap
+import dev.icerock.moko.mvvm.livedata.all
 import dev.icerock.moko.mvvm.livedata.map
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
-
-private val myLazyRepository: Repo by lazy { RepoImpl() }
-
-class RepoImpl : Repo {
-    override val sourceLiveData: LiveData<String> = MutableLiveData(
-        initialValue = buildString {
-            @Suppress("MagicNumber")
-            for (i in 0..1000000) {
-                append("XAXA")
-            }
-        }
-    )
-}
-
-interface Repo {
-    val sourceLiveData: LiveData<String>
-}
 
 class ValidationAllViewModel : ViewModel() {
     val email: MutableLiveData<String> = MutableLiveData("")
     val password: MutableLiveData<String> = MutableLiveData("")
 
-    private val isEmailValid: LiveData<Boolean> =
-        email.flatMap { myLazyRepository.sourceLiveData }.map { it + it }.map { it.isNotEmpty() }
-    private val isPasswordValid: LiveData<Boolean> =
-        MutableLiveData(false) // password.map { it.isNotEmpty() }
+    private val isEmailValid: LiveData<Boolean> = email.map { it.isNotEmpty() }
+    private val isPasswordValid: LiveData<Boolean> = password.map { it.isNotEmpty() }
     val isLoginButtonEnabled: LiveData<Boolean> =
-        isEmailValid // MutableLiveData(false)//listOf(isEmailValid, isPasswordValid).all(true)
+        listOf(isEmailValid, isPasswordValid).all(value = true)
 }
