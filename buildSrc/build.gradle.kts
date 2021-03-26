4585a7cc89e98a223caeccc61d3467d56535f236
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-03-26 16:56:39 +0700

    downgrade android gradle plugin for use project from IDEA

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,5 +22,5 @@ repositories {
 dependencies {
     implementation("dev.icerock:mobile-multiplatform:0.9.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-    implementation("com.android.tools.build:gradle:4.1.2")
+    implementation("com.android.tools.build:gradle:4.0.2")
 }
