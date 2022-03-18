e66ccd24fa850bfa5a9bbee2df24f243334b20fb
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-03-18 22:46:54 +0700

    #168 open details screen swiftui

--- sample-apps/iosApp/iosApp/BookListViewBinding.swift
+++ sample-apps/iosApp/iosApp/BookListViewBinding.swift
@@ -10,32 +10,47 @@ import Foundation
 import SwiftUI
 import shared
 
+func createViewModel() -> BookListViewModel {
+    return BookListViewModel().start()
+}
 
 struct BookListView: View {
-    @ObservedObject var viewModel: BookListViewModel = BookListViewModel()
+    @ObservedObject var viewModel: BookListViewModel = createViewModel()
+    @State private var detailOpened: Bool = false
+    @State private var detailBookId: Int32? = nil
     
     var body: some View {
-        BookListViewBody(
-            state: viewModel.state(
-                \.state,
-                equals: { $0 === $1 },
-                mapper: { BookListViewModelStateKs($0) }
-            ),
-            onRetryPressed: {
-                viewModel.onRetryPressed()
+        ZStack {
+            NavigationLink(isActive: $detailOpened) {
+                if let id = detailBookId {
+                    BookDetailsView(id: id)
+                } else {
+                    EmptyView()
+                }
+            } label: {
+                EmptyView()
+                    .hidden()
             }
-        ).onAppear {
-            viewModel.start()
-        }.onDisappear {
-            viewModel.onCleared()
-        }.onReceive(publisher(viewModel.actions)) { action in
-            let action = BookListViewModelActionKs(action)
-            switch(action) {
-            case .routeToBookDetails(let data):
-                print(data.id)
-            case .openUrl(let data):
-                UIApplication.shared.open(URL(string: data.url)!)
-            }
-        }.navigationTitle("Books")
+
+            BookListViewBody(
+                state: viewModel.state(
+                    \.state,
+                    equals: { $0 === $1 },
+                    mapper: { BookListViewModelStateKs($0) }
+                ),
+                onRetryPressed: {
+                    viewModel.onRetryPressed()
+                }
+            ).onReceive(publisher(viewModel.actions)) { action in
+                let action = BookListViewModelActionKs(action)
+                switch(action) {
+                case .routeToBookDetails(let data):
+                    detailBookId = data.id
+                    detailOpened = true
+                case .openUrl(let data):
+                    UIApplication.shared.open(URL(string: data.url)!)
+                }
+            }.navigationTitle("Books")
+        }
     }
 }
--- sample-apps/iosApp/iosApp/ContentView.swift
+++ sample-apps/iosApp/iosApp/ContentView.swift
@@ -46,13 +46,13 @@ struct MainView: View {
 }
 
 struct BookDetailsView: View {
-    let id: String
+    let id: Int32
     
     var body: some View {
         ScrollView {
             VStack {
                 Text("id: ")
-                Text(id)
+                Text("\(id)")
             }
         }.navigationTitle("Book details")
     }
