b0746157012b89b40846cfa7ebf7c95a038e7e80
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-05-03 11:22:26 +0700

    change just for test ci caches

--- mvvm-flow/src/commonMain/kotlin/dev/icerock/moko/mvvm/flow/CMutableStateFlow.kt
+++ mvvm-flow/src/commonMain/kotlin/dev/icerock/moko/mvvm/flow/CMutableStateFlow.kt
@@ -9,3 +9,5 @@ import kotlinx.coroutines.flow.MutableStateFlow
 expect class CMutableStateFlow<T>(flow: MutableStateFlow<T>) : MutableStateFlow<T>
 
 fun <T> MutableStateFlow<T>.cMutableStateFlow(): CMutableStateFlow<T> = CMutableStateFlow(this)
+
+fun <T> MutableStateFlow<T>.test() {}
