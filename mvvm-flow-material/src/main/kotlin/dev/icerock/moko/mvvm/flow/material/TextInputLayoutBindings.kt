da14b6e08d9597cd3dc89dd6174690a54581579f
Konstantin Kolchurin <kkolchurin@icerockdev.com>
2022-10-28 14:17:00 +0700

    add textinputlayout error binding for flow, fix for livedata

--- mvvm-flow-material/src/main/kotlin/dev/icerock/moko/mvvm/flow/material/TextInputLayoutBindings.kt
+++ mvvm-flow-material/src/main/kotlin/dev/icerock/moko/mvvm/flow/material/TextInputLayoutBindings.kt
@@ -2,32 +2,32 @@
  * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package dev.icerock.moko.mvvm.livedata.material
+package dev.icerock.moko.mvvm.flow.material
 
 import androidx.lifecycle.LifecycleOwner
 import com.google.android.material.textfield.TextInputLayout
-import dev.icerock.moko.mvvm.livedata.Closeable
-import dev.icerock.moko.mvvm.livedata.LiveData
-import dev.icerock.moko.mvvm.utils.bind
+import dev.icerock.moko.mvvm.flow.CStateFlow
+import dev.icerock.moko.mvvm.flow.binding.bind
 import dev.icerock.moko.resources.desc.StringDesc
+import kotlinx.coroutines.DisposableHandle
 
 @JvmName("bindErrorString")
-fun TextInputLayout.bindError(
+fun <T : String?> TextInputLayout.bindError(
     lifecycleOwner: LifecycleOwner,
-    liveData: LiveData<String?>
-): Closeable {
-    return liveData.bind(lifecycleOwner) {
+    flow: CStateFlow<T?>
+): DisposableHandle {
+    return flow.bind(lifecycleOwner) {
         error = it
         isErrorEnabled = it != null
     }
 }
 
 @JvmName("bindErrorStringDesc")
-fun TextInputLayout.bindError(
+fun <T : StringDesc?> TextInputLayout.bindError(
     lifecycleOwner: LifecycleOwner,
-    liveData: LiveData<StringDesc?>
-): Closeable {
-    return liveData.bind(lifecycleOwner) {
+    flow: CStateFlow<T?>
+): DisposableHandle {
+    return flow.bind(lifecycleOwner) {
         error = it?.toString(context)
         isErrorEnabled = it != null
     }
