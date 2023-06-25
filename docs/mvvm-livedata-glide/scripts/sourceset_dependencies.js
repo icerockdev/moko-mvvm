3240e5e0d6f0a46372b1bc68d7d0b57db1411f14
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2023-06-26 00:01:39 +0600

    remove docs dir

--- docs/mvvm-livedata-glide/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":mvvm-livedata-glide:dokkaHtml/debug": [], ":mvvm-livedata-glide:dokkaHtml/main": [], ":mvvm-livedata-glide:dokkaHtml/release": []}'
--- docs/sample/android-app/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":sample:android-app:dokkaHtml/debug": [], ":sample:android-app:dokkaHtml/main": [], ":sample:android-app:dokkaHtml/release": []}'
