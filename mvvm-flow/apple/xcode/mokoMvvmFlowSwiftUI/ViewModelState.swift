8c5e7739eff54d4c4afd970c1bf4bc41b66a3fa7
darronschall <darron.schall@gmail.com>
2024-03-15 22:15:09 -0400

    Clarify SwiftUI ViewModel state null semantics
    This simplifies code and cleans up call-site use, and makes it explicit when to use each helper. When your Kotlin ViewModel `StateFlow` has non-optional values, use the `state` helper in SwiftUI. When your `StateFlow` has an optional type and might have `null` values, use `stateNullable` in SwiftUI.
    
    Note: There isn't compiler help when deciding between `state` and `stateNullable` in SwiftUI. Choosing `state` where `stateNullable` is required will cause a runtime crash due to force unwrapping. Take care to use the correct helper.

--- mvvm-flow/apple/xcode/mokoMvvmFlowSwiftUI/ViewModelState.swift
+++ mvvm-flow/apple/xcode/mokoMvvmFlowSwiftUI/ViewModelState.swift
@@ -13,17 +13,17 @@ public extension ObservableObject where Self: ViewModel {
 
     func state<T, R>(
         _ flowKey: KeyPath<Self, CStateFlow<T>>,
-        equals: @escaping (T?, T?) -> Bool,
+        equals: @escaping (T, T) -> Bool,
         mapper: @escaping (T) -> R
     ) -> R {
         let stateFlow: CStateFlow<T> = self[keyPath: flowKey]
-        var lastValue: T? = stateFlow.value
-        
+        var lastValue: T = stateFlow.value!
+
         var disposable: DisposableHandle? = nil
         
         disposable = stateFlow.subscribe(onCollect: { [weak self] value in
-            if !equals(lastValue, value) {
-                lastValue = value
+            if !equals(lastValue, value!) {
+                lastValue = value!
                 self?.objectWillChange.send()
                 disposable?.dispose()
             }
@@ -35,7 +35,7 @@ public extension ObservableObject where Self: ViewModel {
     func state(_ flowKey: KeyPath<Self, CStateFlow<KotlinBoolean>>) -> Bool {
         return state(
             flowKey,
-            equals: { $0?.boolValue == $1?.boolValue },
+            equals: { $0.boolValue == $1.boolValue },
             mapper: { $0.boolValue }
         )
     }
@@ -43,7 +43,7 @@ public extension ObservableObject where Self: ViewModel {
     func state(_ flowKey: KeyPath<Self, CStateFlow<KotlinDouble>>) -> Double {
         return state(
             flowKey,
-            equals: { $0?.doubleValue == $1?.doubleValue },
+            equals: { $0.doubleValue == $1.doubleValue },
             mapper: { $0.doubleValue }
         )
     }
@@ -51,7 +51,7 @@ public extension ObservableObject where Self: ViewModel {
     func state(_ flowKey: KeyPath<Self, CStateFlow<KotlinFloat>>) -> Float {
         return state(
             flowKey,
-            equals: { $0?.floatValue == $1?.floatValue },
+            equals: { $0.floatValue == $1.floatValue },
             mapper: { $0.floatValue }
         )
     }
@@ -59,7 +59,7 @@ public extension ObservableObject where Self: ViewModel {
     func state(_ flowKey: KeyPath<Self, CStateFlow<KotlinInt>>) -> Int {
         return state(
             flowKey,
-            equals: { $0?.intValue == $1?.intValue },
+            equals: { $0.intValue == $1.intValue },
             mapper: { $0.intValue }
         )
     }
@@ -67,7 +67,7 @@ public extension ObservableObject where Self: ViewModel {
     func state(_ flowKey: KeyPath<Self, CStateFlow<KotlinLong>>) -> Int64 {
         return state(
             flowKey,
-            equals: { $0?.int64Value == $1?.int64Value },
+            equals: { $0.int64Value == $1.int64Value },
             mapper: { $0.int64Value }
         )
     }
@@ -84,14 +84,10 @@ public extension ObservableObject where Self: ViewModel {
         return state(
             flowKey,
             equals: { oldValue, newValue in
-                if let oldValue = oldValue {
-                    guard let newValue = newValue as? Array<T> else {
-                        return false
-                    }
-                    return oldValue.isEqual(to: newValue)
-                } else {
-                    return newValue == nil
+                guard let newValue = newValue as? Array<T> else {
+                    return false
                 }
+                return oldValue.isEqual(to: newValue)
             },
             mapper: { $0 as! Array<T> }
         )
